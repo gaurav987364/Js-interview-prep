fabbf49edd999ee5774ad4acf64d58bf04626e6d
gaurav <145903987+gaurav987364@users.noreply.github.com>
2024-11-25 13:08:05 +0530

    ..

--- concept.js
+++ concept.js
@@ -38,7 +38,8 @@ function greet() {
 //!Variable Hoisting
 //?Variables declared with var are hoisted but initialized with undefined. Variables declared with let and const are hoisted but not initialized. They remain in a "{temporal dead zone}" until the declaration is encountered.
 
-
+// variable expression also
+// so my var = undefined, se initialize so it giveundef and output
 console.log(myVar); // Output: undefined
 var myVar = "Hello";
 console.log(myVar); // Output: Hello
@@ -220,3 +221,82 @@ abc();
 //hello
 //hello
 ///heloo
+
+
+
+
+
+//! Js event Loop;
+
+//?Q1 javascript is single threaded - synchronous language and dynamically typed lang. bu it has capablity to handel asyncchoruns tasks using callbacks, async/await, promisies.
+
+//* javascript execution context ;
+//? Global execution context (this);
+//! memory creation phase => isme sirf variable, function or code ki copy banti hai execute nhi hoti
+//! execution phase ==> isme vo copy execute hone ke liye atui hai or ye banati hai ek box jisme hota hai new Enviourment variable + thread jo dubara se in two pahse ko create krte hai memory and execution phaese fir execution se return hokar chije jati hai global execution context pe and vo box delete hojata hai and this cycle runfor every function call();
+//? function execution context
+//? eval execution context
+
+//* let understand with the example
+let value1 = 10;
+let value2 = 5;
+function addSum(val1, val2){
+    let total = val1 + val2;
+    return total;
+}
+let result1 = addSum(value1, value2);
+let result2 = addSum(30,60);
+
+// in above code step by step chije smajho::
+
+// * sbasbe phle banega global execution context (this); code snippt isme ayga ;
+// * ab banega memory allocation/creation phase --> is phase me simply code ka sirf clone banega code execute anhi hoga like : ⬇️
+// let value1 = undefined;
+// let value2 = undefined;
+// addSum = function(){ //only body of function
+//     let total = undefined;
+//     return total;
+// }
+
+// * ab clone ke baad code execution phase me ayga jisme ek box create hoga jisme hogi 2 chije [new envioument variable + thread];
+
+// * ye 2 chije milkar ab same chij ko repeat kregi of creation memory phase and execution phase ⬇️
+
+// * ab execution phase me chala hai jisme chal rhega ki addSum function call hoga and value1, value2 ko pass krega and total ko return hoga total;
+
+// * ye total return hokar jayga sedha upar global execution context ke pass and hamara box delete hojyga , fir again result2 ke liye dubara same process hogi;
+
+
+//! lets understand call stack, other things
+
+//? => Event Loop - It is a single-threaded model that handles asynchronous tasks. It works by having a queue of tasks and a call stack.
+
+//! ham call stack ka working dekh sakte hai console.log me source me jao or udhar snippet banao and fir run and execute krke dekho by adding break points;
+// by making this example
+function one(){
+    console.log("one")
+}
+function two(){
+    console.log("two")
+}
+function three(){
+    console.log("three")
+}
+one();
+two();
+three();
+
+//? output will be : one two three is snippet ko console source me dekho call stack ka working behavoiur then fir thoda complex example dekho for under standing LIFO principal by this snippet
+function foo() {
+    console.log('foo');
+}
+function bar() {
+    foo();
+    console.log('bar');
+}
+bar();
+// bar -> foo -> console.log('foo') -> foo finishes -> console.log('bar') -> bar finishes
+
+
+//call stack works on LIFO(last in first out principal)
+//? jo phle aya bo last me jayga or last wala sabse phle jayga
