ae528d179015ed1cca5472e8a472a9ef1e29dc76
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-03-18 17:30:18 +0530

    async question +1

--- async.js
+++ async.js
@@ -22,3 +22,34 @@ new Promise((resolve)=>{
 });
 console.log(3);
 // 3 1 2 ok
+
+
+
+
+let score = 0;
+async function increaseScore(){
+    score = score + (await 2); //(await 2) is converted into Promise.resolve(2)
+    console.log("first score",score);
+}
+
+increaseScore();
+score = score + 1;
+console.log("second score", score);
+//1 2
+
+async function name(params) {
+    
+};
+
+console.log(name); // [Function: name] ok
+
+
+console.log("1");
+setTimeout(() => {
+    console.log("2");
+}, 0);  
+console.log("3");
+Promise.resolve().then(()=>{
+    console.log("4");
+})
+// 1 3 4 2
